ed69ea777b6a87d936c720819af50ddf869c5aa2
Michael <michael@Michaels-MacBook-Pro-6.local>
2016-03-15 13:58:11 -0700

    Edited theSubscriber model to take advantage of Django's default User model

--- subscriptions/subscribers/models.py
+++ subscriptions/subscribers/models.py
@@ -1,14 +1,26 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from localflavor.us.models import USStateField, USZipCodeField
+from django.contrib.auth.models import User
 
 class Subscriber(models.Model):
-	firstname = models.CharField(max_length=200)
-	lastname = models.CharField(max_length=200)
-	email = models.EmailField(max_length=254)
-	# TODO: Address 
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
 	end_date = DateTimeField('End Date')
-	history = models.ManyToManyField('Transaction')
+	PRINT = 'P'
+	EMAIL = 'E'
+	BOTH = 'B'
+	SUBSCRIPTION_TYPE = (
+		(PRINT, 'Print'),
+		(EMAIL, 'Email'),
+		(BOTH, 'Both')
+		)
+	subscription_type = models.CharField(max_length=1, 
+		choices=SUBSCRIPTION_TYPE, default=PRINT)
+	line1 = models.CharField(max_length=200)
+	line2 = models.CharField(max_length=200)
+	state = USStateField()
+	zip_code = USZipCodeField()
 
 class Package(models.Model):
 	name = models.CharField(max_length=500)
@@ -16,6 +28,7 @@ class Package(models.Model):
 	months = models.IntegerField()
 
 class Transaction(models.Model)
+	subscriber = models.ForeignKey(Subscriber)
 	date = DateTimeField('Transaction Date')
 	paid = models.DecimalField(max_digits=5, decimal_places=2)
 	received = models.CharField(max_length=200)
